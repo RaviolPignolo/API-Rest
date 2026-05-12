a857fd79faffbc1ef10bfecc621cfbf2f33766ea
LucianoGil05 <ldgil.udc.edu.ar>
2026-05-12 17:23:57 -0300

    Creacion de archivo base para Controller:estructuras de las tareas a realizar

---
+++ controllers/controllersTareas.js
@@ -0,0 +1,18 @@
+// importacion  de la base de datos de tareas desde models
+const tareas = require('../models/modeloTareas');
+
+
+exports.listarTareas = (req, res) => {
+// tarea que  enlista  las tareas
+
+};
+
+
+exports.obtenerTareaId = (req, res) =>{
+    // busca una tarea  por su id (mediante parametros de url)
+
+}
+
+exports.crearTarea = (req, res) => {
+    // se crea la tarea  q se quiere agregar a la lista
+};
