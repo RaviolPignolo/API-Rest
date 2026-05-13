95a792d442e5589691ab0f823286f757677cf8e3
LucianoGil05 <ldgil.udc.edu.ar>
2026-05-13 01:04:26 -0300

    Creacion endpoint para buscar una tarea por ID

--- controllers/controllersTareas.js
+++ controllers/controllersTareas.js
@@ -8,10 +8,19 @@ exports.listarTareas = (req, res) => {
 };
 
 
+// busca una tarea  por su id (mediante parametros de url)
 exports.obtenerTareaId = (req, res) =>{
-    // busca una tarea  por su id (mediante parametros de url)
-
+    // busca en tareas el objeto donde el id coincide con el de la url
+    // parseInt convierte el texto de la URL a numerp y req.params.id obtiene el valor
+    const tarea = tareas.find(t => t.id === parseInt(req.params.id));
+    // sino arroja resultado, lanza menasaje de error y devuelve el estado 404 de error
+    if (!tarea) {
+        return res.status(404).json({ mensaje: 'Tarea no encontrada' });
+    }   
+    // i la encuentra la devuelve en formato json
+    res.json(tarea);    
 }
+
 
 exports.crearTarea = (req, res) => {
     // se crea la tarea  q se quiere agregar a la lista
